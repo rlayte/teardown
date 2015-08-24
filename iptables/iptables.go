9e95aa030d6fdaa9111c99551cb9ae259af30333
Charles Chamberlain <cchamberlain@uchicago.edu>
2015-08-24 00:42:54 -0400

    More functionality for nemisis and iptables

--- iptables/iptables.go
+++ iptables/iptables.go
@@ -17,15 +17,24 @@ func iptables(args string) []byte {
 	return out
 }
 
-func Partition(nodes []string, position int) {
+func PartitionLevel(nodes []string, position int) {
 	for i := 0; i < position; i++ {
 		for j := position; j < len(nodes); j++ {
 			n1 := nodes[i]
 			n2 := nodes[j]
-
-			iptables(fmt.Sprintf("-A INPUT -j DROP -s %s -d %s", n1, n2))
+			Deny(n1, n2)
 		}
 	}
+}
+
+func DenyDirection(incoming string, outgoing string) {
+	// Should we be doing this for OUTPUT too?
+	iptables(fmt.Sprintf("-A INPUT -j DROP -s %s -d %s", incoming, outgoing))
+}
+
+func Deny(incoming string, outgoing string) {
+	DenyDirection(incoming, outgoing)
+	DenyDirection(outgoing, incoming)
 }
 
 func Heal() {
--- nemesis.go
+++ nemesis.go
@@ -1,6 +1,7 @@
 package teardown
 
 import (
+	"fmt"
 	"math/rand"
 
 	"github.com/rlayte/teardown/iptables"
@@ -9,10 +10,10 @@ import (
 type Nemesis interface {
 	PartitionHalf()
 	PartitionRandom()
-	PartitionSingle(node int)
+	PartitionSingle(position int)
 	PartitionLeader()
 	Bridge()
-	Fail(node int)
+	Fail(position int)
 	FailRandom()
 	FailLeader()
 	Heal()
@@ -22,33 +23,60 @@ type LocalNemesis struct {
 	nodes []string
 }
 
+func (n *LocalNemesis) positionToAddress(position int) string {
+	if position >= len(n.nodes) {
+		panic(fmt.Sprintf(
+			"Position %d too large for set of %d",
+			position,
+			len(n.nodes),
+		))
+	}
+	return n.nodes[position]
+}
+
 func (n *LocalNemesis) PartitionHalf() {
 	half := len(n.nodes) / 2
-	iptables.Partition(n.nodes, half)
+	iptables.PartitionLevel(n.nodes, half)
 }
 
 func (n *LocalNemesis) PartitionRandom() {
 	position := rand.Intn(len(n.nodes))
-	iptables.Partition(n.nodes, position)
+	iptables.PartitionLevel(n.nodes, position)
 }
 
-func (n *LocalNemesis) PartitionSingle(node int) {
+// What does this do?
+func (n *LocalNemesis) PartitionSingle(n1 int) {
 }
 
 func (n *LocalNemesis) PartitionLeader() {
 }
 
 func (n *LocalNemesis) Bridge() {
+	bridge_index := len(n.nodes) / 2 // Fails with empty list.
+	for _, n1 := range n.nodes[:bridge_index] {
+		for _, n2 := range n.nodes[bridge_index+1:] {
+			iptables.Deny(n1, n2)
+		}
+	}
 }
 
-func (n *LocalNemesis) Fail(node int) {
+func (n *LocalNemesis) Fail(position int) {
+	n1 := n.positionToAddress(position)
+	for _, n2 := range n.nodes {
+		if n1 != n2 {
+			iptables.Deny(n1, n2)
+		}
+	}
 }
 
 func (n *LocalNemesis) FailRandom() {
+	position := rand.Intn(len(n.nodes))
+	n.Fail(position)
 }
 
 func (n *LocalNemesis) FailLeader() {
 }
 
 func (n *LocalNemesis) Heal() {
+	iptables.Heal()
 }
